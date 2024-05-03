2cbc816b6333f218ca8f2ed75a7a7fadb0937b51
shirara1016 <shirara1016@gmail.com>
2024-05-03 20:25:09 +0900

    additional oc experiment

--- execute.sh
+++ execute.sh
@@ -5,7 +5,7 @@
 #PBS -j oe
 #PBS -o path_for_logs
 #PBS -N pl
-#PBS -q cpu7-9
+#PBS -q cpu4-6
 #PBS -l select=1:ncpus=32:mem=128gb:ompthreads=1
 
 cd $PBS_O_WORKDIR
--- exp_tpr_oc.sh
+++ exp_tpr_oc.sh
@@ -3,7 +3,7 @@
 file="main_experiment.py"
 
 for option in op1 op2 op1cv op2cv op12cv; do
-    for seed in $(seq 0 3); do
+    for seed in $(seq 0 9); do
         for delta in 0.2 0.4 0.6 0.8; do
             qsub \
             -v MYARGS="--seed $seed --delta $delta --option $option --oc oc",MYFILE=$file \
