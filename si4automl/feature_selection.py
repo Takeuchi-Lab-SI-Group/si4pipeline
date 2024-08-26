15cc4ffc44fe91f56d47a48aaf31792c8bcecda9
shirara1016 <shirara1016@gmail.com>
2024-08-26 18:03:52 +0900

    update feature selection entities

--- si4automl/feature_selection.py
+++ si4automl/feature_selection.py
@@ -1,41 +1,18 @@
 """Module containing feature selection methods."""
 
-from typing import ClassVar
+from typing import cast
 
 import numpy as np
 import sklearn.linear_model as lm  # type: ignore[import]
 from sicore import polytope_below_zero  # type: ignore[import]
 
-from si4automl.base_component import FeatureMatrix, ResponseVector, SelectedFeatures
-
 
 class FeatureSelection:
-    """An abstract class for feature selection."""
+    """A class for feature selection methods."""
 
-    instance_counter: ClassVar[dict[str, int]] = {}
-
-    def __init__(
-        self,
-        name: str,
-        parameters: float,
-        candidates: list[float] | list[int] | None,
-    ) -> None:
+    def __init__(self, parameter: float) -> None:
         """Initialize the FeatureSelection object."""
-        self.parameters = parameters
-        self.candidates = candidates
-        FeatureSelection.instance_counter.setdefault(name, 0)
-        self.name = f"{name}_{FeatureSelection.instance_counter[name]}"
-        FeatureSelection.instance_counter[name] += 1
-
-    def __call__(
-        self,
-        feature_matrix: FeatureMatrix,
-        response_vector: ResponseVector,
-    ) -> SelectedFeatures:
-        """Perform the feature selection."""
-        pl_structure = feature_matrix.pl_structure | response_vector.pl_structure
-        pl_structure.update(self.name, self)
-        return SelectedFeatures(pl_structure)
+        self.parameter = parameter
 
     def select_features(
         self,
@@ -48,10 +25,10 @@ class FeatureSelection:
         raise NotImplementedError
 
     def reset_intervals(self) -> None:
-        """Reset the intervals for the selective inference."""
+        """Reset the intervals to execute the selective inference."""
         self.intervals: dict[
-            int | None,
-            dict[int | None, dict[tuple[float, float], tuple[list[int], list[int]]]],
+            int,
+            dict[tuple[float, float], tuple[list[int], list[int]]],
         ] = {}
 
     def load_intervals(
@@ -59,28 +36,14 @@ class FeatureSelection:
         z: float,
         l: float,
         u: float,
-        candidate_id: int | None = None,
-        mask_id: int | None = None,
+        mask_id: int = -1,
     ) -> tuple[list[int], list[int], float, float] | None:
-        """Load the intervals for the selective inference."""
-        # if candidate_id is None and mask_id is None:
-        #     self.intervals.setdefault(None, {})
-        #     items = self.intervals[None].items()
-        # elif candidate_id is not None and mask_id is not None:
-        #     self.intervals.setdefault(candidate_id, {})
-        #     self.intervals[candidate_id].setdefault(mask_id, {})
-        #     items = self.intervals[candidate_id][mask_id].items()
-        # else:
-        #     raise ValueError  # candidate_id and mask_id must be both None or not None
-        self.intervals.setdefault(candidate_id, {})
-        self.intervals[candidate_id].setdefault(mask_id, {})
-        items = self.intervals[candidate_id][mask_id].items()
-
-        for interval, indexes in items:
+        """Load the intervals to execute the selective inference."""
+        for interval, indexes in self.intervals.setdefault(mask_id, {}).items():
             if interval[0] < z < interval[1]:
                 M, O = indexes
-                l = np.max([l, interval[0]])
-                u = np.min([u, interval[1]])
+                l = np.max([l, interval[0]]).item()
+                u = np.min([u, interval[1]]).item()
                 return M, O, l, u
         return None
 
@@ -90,17 +53,10 @@ class FeatureSelection:
         u: float,
         M: list[int],
         O: list[int],
-        candidate_id: int | None = None,
-        mask_id: int | None = None,
+        mask_id: int = -1,
     ) -> None:
-        """Save the intervals for the selective inference."""
-        # if candidate_id is None and mask_id is None:
-        #     self.intervals[None][(l, u)] = (M, O)
-        # elif candidate_id is not None and mask_id is not None:
-        #     self.intervals[candidate_id][mask_id][(l, u)] = (M, O)
-        # else:
-        #     raise ValueError("candidate_id and mask_id must be both None or not None")
-        self.intervals[candidate_id][mask_id][(l, u)] = (M, O)
+        """Save the intervals to execute the selective inference."""
+        self.intervals[mask_id][(l, u)] = (M, O)
 
     def perform_si(
         self,
@@ -112,21 +68,18 @@ class FeatureSelection:
         detected_outliers: list[int],
         l: float,
         u: float,
-        candidate_id: int | None = None,
-        mask_id: int | None = None,
+        mask_id: int = -1,
     ) -> tuple[list[int], list[int], float, float]:
         """Perform the selective inference."""
         raise NotImplementedError
 
 
 class StepwiseFeatureSelection(FeatureSelection):
-    def __init__(
-        self,
-        name="stepwise_feature_selection",
-        parameters=None,
-        candidates=None,
-    ):
-        super().__init__(name, parameters, candidates)
+    """A class for the stepwise feature selection method."""
+
+    def __init__(self, parameter: int) -> None:
+        """Initialize the StepwiseFeatureSelection object."""
+        super().__init__(parameter)
 
     def select_features(
         self,
@@ -135,6 +88,7 @@ class StepwiseFeatureSelection(FeatureSelection):
         selected_features: list[int],
         detected_outliers: list[int],
     ) -> list[int]:
+        """Perform the feature selection."""
         X, y = feature_matrix, response_vector
         M, O = selected_features, detected_outliers
 
@@ -144,14 +98,15 @@ class StepwiseFeatureSelection(FeatureSelection):
 
         # initialize
         min_mse = np.inf
-        active_set = []
+        active_set: list[int] = []
         inactive_set = list(range(X.shape[1]))
 
         # stepwise feature selection
-        for _ in range(self.parameters):
+        self.parameter = cast(int, self.parameter)
+        for _ in range(self.parameter):
             mse_list = []
             for inactive_feature in inactive_set:
-                active_temp = active_set + [inactive_feature]
+                active_temp = [*active_set, inactive_feature]
                 X_active_temp = X[:, active_temp]
                 r = (
                     np.identity(X.shape[0])
@@ -171,8 +126,7 @@ class StepwiseFeatureSelection(FeatureSelection):
             else:
                 break
 
-        M = [M[i] for i in active_set]
-        return M
+        return [M[i] for i in active_set]
 
     def perform_si(
         self,
@@ -184,10 +138,10 @@ class StepwiseFeatureSelection(FeatureSelection):
         detected_outliers: list[int],
         l: float,
         u: float,
-        candidate_id: int | None = None,
-        mask_id: int | None = None,
-    ) -> tuple[list[int], list[int], float, float]:  # type: ignore
-        results = self.load_intervals(z, l, u, candidate_id, mask_id)
+        mask_id: int = -1,
+    ) -> tuple[list[int], list[int], float, float]:
+        """Perform the selective inference."""
+        results = self.load_intervals(z, l, u, mask_id)
         if results is not None:
             return results
 
@@ -202,14 +156,15 @@ class StepwiseFeatureSelection(FeatureSelection):
 
         # initialize
         min_mse = np.inf
-        active_set = []
+        active_set: list[int] = []
         inactive_set = list(range(X.shape[1]))
 
         # stepwise feature selection
-        for _ in range(self.parameters):
+        self.parameter = cast(int, self.parameter)
+        for _ in range(self.parameter):
             mse_list = []
             for inactive_feature in inactive_set:
-                active_temp = active_set + [inactive_feature]
+                active_temp = [*active_set, inactive_feature]
                 X_active_temp = X[:, active_temp]
                 r = (
                     np.identity(X.shape[0])
@@ -232,7 +187,7 @@ class StepwiseFeatureSelection(FeatureSelection):
         l_list, u_list = [l], [u]
         inactive_set = list(range(X.shape[1]))
 
-        for i in range(self.parameters):
+        for i in range(self.parameter):
             X_active_k = X[:, active_set[0 : i + 1]]
             mse_active_k = (
                 np.identity(X.shape[0])
@@ -258,19 +213,22 @@ class StepwiseFeatureSelection(FeatureSelection):
                         u_list.append(right)
                         break
 
-        l = np.max(l_list)
-        u = np.min(u_list)
-        assert l < z < u, "l < z < u is not satisfied"
+        l = np.max(l_list).item()
+        u = np.min(u_list).item()
+        assert l < z < u
 
         M = [M[i] for i in active_set]
 
-        self.save_intervals(l, u, M, O, candidate_id, mask_id)
+        self.save_intervals(l, u, M, O, mask_id)
         return M, O, l, u
 
 
 class MarginalScreening(FeatureSelection):
-    def __init__(self, name="marginal_screening", parameters=None, candidates=None):
-        super().__init__(name, parameters, candidates)
+    """A class for the marginal screening method."""
+
+    def __init__(self, parameter: int) -> None:
+        """Initialize the MarginalScreening object."""
+        super().__init__(parameter)
 
     def select_features(
         self,
@@ -279,6 +237,7 @@ class MarginalScreening(FeatureSelection):
         selected_features: list[int],
         detected_outliers: list[int],
     ) -> list[int]:
+        """Perform the feature selection."""
         X, y = feature_matrix, response_vector
         M, O = selected_features, detected_outliers
 
@@ -290,9 +249,9 @@ class MarginalScreening(FeatureSelection):
         XTy_abs = np.abs(X.T @ y).flatten()
         sort_XTy_abs = np.argsort(XTy_abs)[::-1]
 
-        active_set = sort_XTy_abs[: self.parameters]
-        M = [M[i] for i in active_set]
-        return M
+        self.parameter = cast(int, self.parameter)
+        active_set = sort_XTy_abs[: self.parameter]
+        return [M[i] for i in active_set]
 
     def perform_si(
         self,
@@ -304,10 +263,10 @@ class MarginalScreening(FeatureSelection):
         detected_outliers: list[int],
         l: float,
         u: float,
-        candidate_id: int | None = None,
-        mask_id: int | None = None,
+        mask_id: int = -1,
     ) -> tuple[list[int], list[int], float, float]:
-        results = self.load_intervals(z, l, u, candidate_id, mask_id)
+        """Perform the selective inference."""
+        results = self.load_intervals(z, l, u, mask_id)
         if results is not None:
             return results
 
@@ -323,8 +282,9 @@ class MarginalScreening(FeatureSelection):
         XTyz_abs = np.abs(X.T @ yz).flatten()
         sort_XTyz_abs = np.argsort(XTyz_abs)[::-1]
 
-        active_set = sort_XTyz_abs[: self.parameters]
-        inactive_set = sort_XTyz_abs[self.parameters :]
+        self.parameter = cast(int, self.parameter)
+        active_set = sort_XTyz_abs[: self.parameter]
+        inactive_set = sort_XTyz_abs[self.parameter :]
 
         left_list = []
         right_list = []
@@ -353,7 +313,7 @@ class MarginalScreening(FeatureSelection):
         for left, right in zip(left_list, right_list, strict=False):
             if np.around(left, 5) == 0:
                 if right <= 0:
-                    raise ValueError("l must be less than u")
+                    raise ValueError  # "l must be less than u"
                 continue
             term = right / left
             if left > 0:
@@ -361,19 +321,22 @@ class MarginalScreening(FeatureSelection):
             else:
                 l_list.append(term)
 
-        l = np.max(l_list)
-        u = np.min(u_list)
-        assert l < z < u, "l < z < u is not satisfied"
+        l = np.max(l_list).item()
+        u = np.min(u_list).item()
+        assert l < z < u
 
         M = [M[i] for i in active_set]
 
-        self.save_intervals(l, u, M, O, candidate_id, mask_id)
+        self.save_intervals(l, u, M, O, mask_id)
         return M, O, l, u
 
 
 class Lasso(FeatureSelection):
-    def __init__(self, name="lasso", parameters=None, candidates=None):
-        super().__init__(name, parameters, candidates)
+    """A class for the lasso method."""
+
+    def __init__(self, parameter: float) -> None:
+        """Initialize the Lasso object."""
+        super().__init__(parameter)
 
     def select_features(
         self,
@@ -382,6 +345,7 @@ class Lasso(FeatureSelection):
         selected_features: list[int],
         detected_outliers: list[int],
     ) -> list[int]:
+        """Perform the feature selection."""
         X, y = feature_matrix, response_vector
         M, O = selected_features, detected_outliers
 
@@ -391,15 +355,14 @@ class Lasso(FeatureSelection):
 
         # lasso
         lasso = lm.Lasso(
-            alpha=self.parameters,
+            alpha=self.parameter,
             fit_intercept=False,
             max_iter=5000,
             tol=1e-10,
         )
         lasso.fit(X, y)
         active_set = np.where(lasso.coef_ != 0)[0]
-        M = [M[i] for i in active_set]
-        return M
+        return [M[i] for i in active_set]
 
     def perform_si(
         self,
@@ -411,10 +374,10 @@ class Lasso(FeatureSelection):
         detected_outliers: list[int],
         l: float,
         u: float,
-        candidate_id: int | None = None,
-        mask_id: int | None = None,
+        mask_id: int = -1,
     ) -> tuple[list[int], list[int], float, float]:
-        results = self.load_intervals(z, l, u, candidate_id, mask_id)
+        """Perform the selective inference."""
+        results = self.load_intervals(z, l, u, mask_id)
         if results is not None:
             return results
 
@@ -428,7 +391,7 @@ class Lasso(FeatureSelection):
         a, b = np.delete(a, O), np.delete(b, O)
 
         lasso = lm.Lasso(
-            alpha=self.parameters,
+            alpha=self.parameter,
             fit_intercept=False,
             max_iter=5000,
             tol=1e-10,
@@ -445,17 +408,17 @@ class Lasso(FeatureSelection):
         Pm = X_active @ X_active_plus
 
         A0_plus = (X_inactive.T @ (np.identity(X.shape[0]) - Pm)) / (
-            self.parameters * X.shape[0]
+            self.parameter * X.shape[0]
         )
         A0_minus = (-X_inactive.T @ (np.identity(X.shape[0]) - Pm)) / (
-            self.parameters * X.shape[0]
+            self.parameter * X.shape[0]
         )
         b0_plus = np.ones(X_inactive.shape[1]) - X_inactive.T @ X_active_plus.T @ signs
         b0_minus = np.ones(X_inactive.shape[1]) + X_inactive.T @ X_active_plus.T @ signs
 
         A1 = -np.diag(signs) @ X_active_plus
         b1 = (
-            -self.parameters
+            -self.parameter
             * X.shape[0]
             * np.diag(signs)
             @ np.linalg.inv(X_active.T @ X_active)
@@ -476,7 +439,7 @@ class Lasso(FeatureSelection):
         for left, right in zip(left_list, right_list, strict=False):
             if np.around(left, 5) == 0:
                 if right <= 0:
-                    raise ValueError("l must be less than u")
+                    raise ValueError  # l must be less than u
                 continue
             term = right / left
             if left > 0:
@@ -484,84 +447,11 @@ class Lasso(FeatureSelection):
             else:
                 l_list.append(term)
 
-        l = np.max(l_list)
-        u = np.min(u_list)
-        assert l < z < u, "l < z < u is not satisfied"
+        l = np.max(l_list).item()
+        u = np.min(u_list).item()
+        assert l < z < u
 
         M = [M[i] for i in active_set]
 
-        self.save_intervals(l, u, M, O, candidate_id, mask_id)
+        self.save_intervals(l, u, M, O, mask_id)
         return M, O, l, u
-
-    def compute_quotient(self, numerator, denominator):
-        if denominator == 0:
-            return np.inf
-        quotient = float(numerator / denominator)
-        if quotient <= 0:
-            return np.inf
-        return quotient
-
-
-class ElasticNet(FeatureSelection):
-    def __init__(self, name="elastic_net", parameters=None, candidates=None):
-        super().__init__(name, parameters, candidates)
-
-
-class Lars(FeatureSelection):
-    def __init__(self, name="lars", parameters=None, candidates=None):
-        super().__init__(name, parameters, candidates)
-
-
-def stepwise_feature_selection(
-    feature_matrix,
-    response_vector,
-    parameters=10,
-    candidates=None,
-):
-    return StepwiseFeatureSelection(parameters=parameters, candidates=candidates)(
-        feature_matrix,
-        response_vector,
-    )
-
-
-def stepwise_feature_selection_with_aic(
-    feature_matrix,
-    response_vector,
-    parameters=None,
-    candidates=None,
-):
-    return StepwiseFeatureSelectionWithAIC(
-        parameters=parameters,
-        candidates=candidates,
-    )(
-        feature_matrix,
-        response_vector,
-    )
-
-
-def marginal_screening(feature_matrix, response_vector, parameters=10, candidates=None):
-    return MarginalScreening(parameters=parameters, candidates=candidates)(
-        feature_matrix,
-        response_vector,
-    )
-
-
-def lasso(feature_matrix, response_vector, parameters=0.1, candidates=None):
-    return Lasso(parameters=parameters, candidates=candidates)(
-        feature_matrix,
-        response_vector,
-    )
-
-
-def elastic_net(feature_matrix, response_vector, parameters=None, candidates=None):
-    return ElasticNet(parameters=parameters, candidates=candidates)(
-        feature_matrix,
-        response_vector,
-    )
-
-
-def lars(feature_matrix, response_vector, parameters=None, candidates=None):
-    return Lars(parameters=parameters, candidates=candidates)(
-        feature_matrix,
-        response_vector,
-    )
