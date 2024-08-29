38605d64c36d087196154af245bbe4237c91ef75
shirara1016 <shirara1016@gmail.com>
2024-08-29 21:14:57 +0900

    implement plot function for estimated variance experiment

--- experiment/summary.py
+++ experiment/summary.py
@@ -15,21 +15,20 @@ from experiment.utils import Results
 def plot_main(option: str, mode: str) -> None:
     """Plot the results of the experiments."""
     values: list[float]
-    ylabel, is_null, num_seeds = "Type I Error Rate", True, 4
+    ylabel, is_null, num_seeds = "Type I Error Rate", True, 9
+    fig_path = Path("figures/main") / f"fpr_{option}_{mode}.pdf"
     match mode:
         case "n":
             values = [100, 200, 300, 400]
-            result_name = lambda value, seed: f"{value}_20_0.0_{seed}.pkl"  # noqa: E731
-            fig_path = Path("figures/main") / f"fpr_{option}_n.pdf"
+            result_name = lambda value, seed: f"{value}_20_0.0_{seed}.pkl"
             xlabel = "number of samples"
         case "d":
             values = [10, 20, 30, 40]
-            result_name = lambda value, seed: f"200_{value}_0.0_{seed}.pkl"  # noqa: E731
-            fig_path = Path("figures/main") / f"fpr_{option}_d.pdf"
+            result_name = lambda value, seed: f"200_{value}_0.0_{seed}.pkl"
             xlabel = "number of features"
         case "delta":
             values = [0.2, 0.4, 0.6, 0.8]
-            result_name = lambda value, seed: f"200_20_{value}_{seed}.pkl"  # noqa: E731
+            result_name = lambda value, seed: f"200_20_{value}_{seed}.pkl"
             fig_path = Path("figures/main") / f"tpr_{option}.pdf"
             xlabel, ylabel, is_null, num_seeds = "signal", "Power", False, 1
 
@@ -59,12 +58,12 @@ def plot_time(mode: str) -> None:
     match mode:
         case "n":
             values = [100, 200, 300, 400]
-            result_name = lambda value, seed: f"{value}_20_0.0_{seed}.pkl"  # noqa: E731
+            result_name = lambda value, seed: f"{value}_20_0.0_{seed}.pkl"
             fig_path = Path("figures/time") / "time_n.pdf"
             xlabel = "number of samples"
         case "d":
             values = [10, 20, 30, 40]
-            result_name = lambda value, seed: f"200_{value}_0.0_{seed}.pkl"  # noqa: E731
+            result_name = lambda value, seed: f"200_{value}_0.0_{seed}.pkl"
             fig_path = Path("figures/time") / "time_d.pdf"
             xlabel = "number of features"
 
@@ -152,12 +151,12 @@ def print_real(option: str) -> None:
 
 
 def plot_robust_non_gaussian(option: str, alpha: float = 0.05) -> None:
-    """Plot the results of the robustness experiments."""
+    """Plot the results of the robustness experiments for non-Gaussian noise."""
     figure = SummaryFigure(xlabel="Wasserstein Distance", ylabel="Type I Error Rate")
     num_seeds = 1
 
     for rv_name in ["skewnorm", "exponnorm", "gennormsteep", "gennormflat", "t"]:
-        for distance in [0.01, 0.02, 0.03]:
+        for distance in [0.01, 0.02, 0.03, 0.04]:
             results = Results()
             for seed in range(num_seeds):
                 path = (
@@ -190,6 +189,52 @@ def plot_robust_non_gaussian(option: str, alpha: float = 0.05) -> None:
     )
 
 
+def plot_robust_estimated(option: str, mode: str) -> None:
+    """Plot the results of the robustness experiments for estimated variance."""
+    match mode:
+        case "n":
+            values = [100, 200, 300]
+            result_name = lambda value, seed: f"{option}_{value}_20_{seed}.pkl"
+            fig_path = Path("figures/robust") / f"{option}_estimated_n.pdf"
+            xlabel = "number of samples"
+        case "d":
+            values = [10, 20, 30, 40]
+            result_name = lambda value, seed: f"{option}_200_{value}_{seed}.pkl"
+            xlabel = "number of features"
+
+    num_seeds = 1
+    figure = SummaryFigure(xlabel=xlabel, ylabel="Type I Error Rate")
+    for value in values:
+        results = Results()
+        for seed in range(num_seeds):
+            path = Path("results_robust/estimated") / result_name(value, seed)
+            with path.open("rb") as f:
+                results += pickle.load(f)
+        assert len(results.p_values) == num_seeds * 1000
+
+        for alpha in [0.05, 0.01, 0.10]:
+            figure.add_results(
+                results.p_values,
+                label=f"alpha={alpha:.2f}",
+                xloc=value,
+                alpha=alpha,
+            )
+
+    figure.add_red_line(value=0.05, label="significance levels")
+    figure.add_red_line(value=0.01)
+    figure.add_red_line(value=0.10)
+
+    fig_path = Path("figures/robust") / f"{option}_estimated_{mode}.pdf"
+    fig_path.parent.mkdir(parents=True, exist_ok=True)
+    figure.plot(
+        fig_path,
+        fontsize=16,
+        legend_loc="upper left",
+        ylim=(0.0, 0.2),
+        yticks=[0.0, 0.01, 0.05, 0.10, 0.15, 0.2],
+    )
+
+
 if __name__ == "__main__":
     with ProcessPoolExecutor(max_workers=32) as executor:
         for arg in product(["op1", "op2", "all_cv"], ["n", "d", "delta"]):
@@ -213,3 +258,5 @@ if __name__ == "__main__":
         executor.submit(print_real, "all_cv")
         for arg_ in [("all_cv", 0.05), ("all_cv", 0.01)]:
             executor.submit(plot_robust_non_gaussian, *arg_)
+        for arg in product(["all_cv"], ["n", "d"]):
+            executor.submit(plot_robust_estimated, *arg)
