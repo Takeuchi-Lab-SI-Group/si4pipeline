45abe2921f48c4d34f182d080d5bf793d0ce4511
shirara1016 <shirara1016@gmail.com>
2024-08-30 15:18:18 +0900

    update results class and add serial and parallel pipeline options

--- experiment/utils.py
+++ experiment/utils.py
@@ -3,8 +3,12 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
 
 from typing_extensions import Self
+
+if TYPE_CHECKING:
+    from sicore import SelectiveInferenceResult  # type: ignore[import]
 
 from si4automl import (
     PipelineManager,
@@ -28,24 +32,21 @@ from si4automl import (
 class Results:
     """Results dataclass for the data analysis pipeline."""
 
-    p_values: list[float] = field(default_factory=list)
-    naive_p_values: list[float] = field(default_factory=list)
+    results: list[SelectiveInferenceResult] = field(default_factory=list)
     oc_p_values: list[float] = field(default_factory=list)
     times: list[float] = field(default_factory=list)
 
     def __add__(self, other: Results) -> Results:
         """Take union of two results."""
         return Results(
-            p_values=self.p_values + other.p_values,
-            naive_p_values=self.naive_p_values + other.naive_p_values,
+            results=self.results + other.results,
             oc_p_values=self.oc_p_values + other.oc_p_values,
             times=self.times + other.times,
         )
 
     def __iadd__(self, other: Self) -> Self:
         """Take union of two results in place."""
-        self.p_values += other.p_values
-        self.naive_p_values += other.naive_p_values
+        self.results += other.results
         self.oc_p_values += other.oc_p_values
         self.times += other.times
         return self
@@ -116,4 +117,50 @@ def option2_multi() -> PipelineManager:
     M1 = stepwise_feature_selection(X, y, [2, 3])
     M2 = lasso(X, y, [0.08, 0.12])
     M = intersection(M1, M2)
+    return construct_pipelines(output=M)
+
+
+def option1_parallel() -> PipelineManager:
+    """Pipeline manager for parallel option1 pipelines."""
+    X, y = initialize_dataset()
+    y = mean_value_imputation(X, y)
+
+    O = soft_ipod(X, y, 0.02)
+    X, y = remove_outliers(X, y, O)
+
+    M = marginal_screening(X, y, 20)
+    X = extract_features(X, M)
+
+    M1 = stepwise_feature_selection(X, y, 5)
+    M2 = lasso(X, y, 0.06)
+    M = union(M1, M2)
+
+    X = extract_features(X, M)
+
+    M1 = stepwise_feature_selection(X, y, 3)
+    M2 = lasso(X, y, 0.08)
+    M = union(M1, M2)
+    return construct_pipelines(output=M)
+
+
+def option1_serial() -> PipelineManager:
+    """Pipeline manager for serial option1 pipelines."""
+    X, y = initialize_dataset()
+    y = mean_value_imputation(X, y)
+
+    O = soft_ipod(X, y, 0.02)
+    X, y = remove_outliers(X, y, O)
+
+    M = marginal_screening(X, y, 20)
+    X = extract_features(X, M)
+
+    M = stepwise_feature_selection(X, y, 10)
+    X = extract_features(X, M)
+
+    M = lasso(X, y, 0.04)
+    X = extract_features(X, M)
+
+    M1 = stepwise_feature_selection(X, y, 3)
+    M2 = lasso(X, y, 0.08)
+    M = union(M1, M2)
     return construct_pipelines(output=M)
