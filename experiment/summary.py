dc0f02f49705de1405853b903cc955b93b9b6133
shirara1016 <shirara1016@gmail.com>
2024-08-30 15:50:50 +0900

    incoporate new results class

--- experiment/summary.py
+++ experiment/summary.py
@@ -39,18 +39,163 @@ def plot_main(option: str, mode: str) -> None:
             path = Path(f"results_{option}") / result_name(value, seed)
             with path.open("rb") as f:
                 results += pickle.load(f)
-        assert len(results.p_values) == num_seeds * 1000
+        assert len(results) == num_seeds * 1000
 
-        figure.add_results(results.p_values, label="proposed", xloc=value)
+        figure.add_results(results.results, label="proposed", xloc=value)
         figure.add_results(results.oc_p_values, label="oc", xloc=value)
         if is_null:
-            figure.add_results(results.naive_p_values, label="naive", xloc=value)
+            figure.add_results(results.results, label="naive", xloc=value, naive=True)
 
     if is_null:
         figure.add_red_line(value=0.05, label="significance level")
 
     fig_path.parent.mkdir(parents=True, exist_ok=True)
     figure.plot(fig_path, fontsize=16, legend_loc="upper left")
+
+
+def plot_real(option: str, key: str) -> None:
+    """Plot the results of the real data experiments."""
+    num_seeds = 1
+    figure = SummaryFigure(xlabel="number of samples", ylabel="Power")
+    for n in [100, 150, 200]:
+        results = Results()
+        for seed in range(num_seeds):
+            path = Path("results_real") / f"{option}_{key}_{n}_{seed}.pkl"
+            with path.open("rb") as f:
+                results += pickle.load(f)
+        assert len(results) == num_seeds * 1000
+
+        figure.add_results(results.results, label="proposed", xloc=n)
+        figure.add_results(results.oc_p_values, label="oc", xloc=n)
+
+    fig_path = Path("figures/real") / f"{option}_{key}.pdf"
+    fig_path.parent.mkdir(parents=True, exist_ok=True)
+    figure.plot(fig_path, fontsize=16, legend_loc="upper left")
+
+
+def print_real(option: str) -> None:
+    """Print the results of the real data experiments."""
+    num_seeds = 1
+    keys = [
+        "heating_load",
+        "cooling_load",
+        "gas_turbine",
+        "red_wine",
+        "white_wine",
+        "abalone",
+        "concrete",
+        "housing",
+    ]
+    strings_list = []
+    for n in [100, 150, 200]:
+        string = f"$n={n}$"
+        for key in keys:
+            results = Results()
+            for seed in range(num_seeds):
+                path = Path("results_real") / f"{option}_{key}_{n}_{seed}.pkl"
+                with path.open("rb") as f:
+                    results += pickle.load(f)
+            assert len(results) == num_seeds * 1000
+
+            tpr_ = rejection_rate(results.results, alpha=0.05)
+            oc_tpr_ = rejection_rate(results.oc_p_values, alpha=0.05)
+            tpr = Decimal(str(tpr_)).quantize(
+                Decimal("0.01"),
+                "ROUND_HALF_UP",
+            )
+            oc_tpr = Decimal(str(oc_tpr_)).quantize(
+                Decimal("0.01"),
+                "ROUND_HALF_UP",
+            )
+            string += rf" & \textbf{{{str(tpr)[1:]}}}/{str(oc_tpr)[1:]}"
+        strings_list.append(string)
+
+    strings_path = Path(f"figures/real/{option}.txt")
+    strings_path.parent.mkdir(parents=True, exist_ok=True)
+    with strings_path.open("w") as f:
+        f.write("\n".join(strings_list))
+
+
+def plot_robust_non_gaussian(option: str, alpha: float = 0.05) -> None:
+    """Plot the results of the robustness experiments for non-Gaussian noise."""
+    num_seeds = 10
+    figure = SummaryFigure(xlabel="Wasserstein Distance", ylabel="Type I Error Rate")
+    for rv_name in ["skewnorm", "exponnorm", "gennormsteep", "gennormflat", "t"]:
+        for distance in [0.01, 0.02, 0.03, 0.04]:
+            results = Results()
+            for seed in range(num_seeds):
+                path = (
+                    Path(f"results_robust/{rv_name}")
+                    / f"{option}_{distance}_{seed}.pkl"
+                )
+                with path.open("rb") as f:
+                    results += pickle.load(f)
+            assert len(results) == num_seeds * 1000
+            figure.add_results(
+                results.results,
+                label=rv_name,
+                xloc=distance,
+                alpha=alpha,
+            )
+    figure.add_red_line(value=alpha, label="significance level")
+
+    default_alpha = 0.05
+    fig_path = Path("figures/robust") / f"{option}_non_gaussian_{alpha:.2f}.pdf"
+    fig_path.parent.mkdir(parents=True, exist_ok=True)
+    figure.plot(
+        fig_path,
+        fontsize=13,
+        legend_loc="upper left",
+        ylim=(0.0, 0.2) if alpha == default_alpha else (0.0, 0.04),
+        yticks=[0.0, 0.05, 0.10, 0.15, 0.2]
+        if alpha == default_alpha
+        else [0.0, 0.01, 0.02, 0.03, 0.04],
+    )
+
+
+def plot_robust_estimated(option: str, mode: str) -> None:
+    """Plot the results of the robustness experiments for estimated variance."""
+    match mode:
+        case "n":
+            values = [100, 200, 300, 400]
+            result_name = lambda value, seed: f"{option}_{value}_20_{seed}.pkl"
+            fig_path = Path("figures/robust") / f"{option}_estimated_n.pdf"
+            xlabel = "number of samples"
+        case "d":
+            values = [10, 20, 30, 40]
+            result_name = lambda value, seed: f"{option}_200_{value}_{seed}.pkl"
+            xlabel = "number of features"
+    num_seeds = 10
+    figure = SummaryFigure(xlabel=xlabel, ylabel="Type I Error Rate")
+    for value in values:
+        results = Results()
+        for seed in range(num_seeds):
+            path = Path("results_robust/estimated") / result_name(value, seed)
+            with path.open("rb") as f:
+                results += pickle.load(f)
+        assert len(results) == num_seeds * 1000
+
+        for alpha in [0.05, 0.01, 0.10]:
+            figure.add_results(
+                results.results,
+                label=f"alpha={alpha:.2f}",
+                xloc=value,
+                alpha=alpha,
+            )
+
+    figure.add_red_line(value=0.05, label="significance levels")
+    figure.add_red_line(value=0.01)
+    figure.add_red_line(value=0.10)
+
+    fig_path = Path("figures/robust") / f"{option}_estimated_{mode}.pdf"
+    fig_path.parent.mkdir(parents=True, exist_ok=True)
+    figure.plot(
+        fig_path,
+        fontsize=13,
+        legend_loc="upper left",
+        ylim=(0.0, 0.2),
+        yticks=[0.0, 0.01, 0.05, 0.10, 0.15, 0.2],
+    )
 
 
 def plot_time(mode: str) -> None:
@@ -74,14 +219,14 @@ def plot_time(mode: str) -> None:
     )
 
     for value in values:
-        for option in ["op1", "op1_parallel"]:
+        for option in ["op1"]:  # TODO(shirara): add op1_parallel and op1_serial
             results = Results()
             for seed in range(num_seeds):
                 dir_path = Path("results_time")
                 path = dir_path / result_name(option, value, seed)
                 with path.open("rb") as f:
                     results += pickle.load(f)
-            assert len(results.times) == num_seeds * 100
+            assert len(results.times) == num_seeds * 1000
 
             times = np.array(results.times)
             num_intervals = np.array(
@@ -114,177 +259,28 @@ def plot_time(mode: str) -> None:
         figure.plot(fig_path, fontsize=16, legend_loc="upper left", ylim=None)
 
 
-def plot_real(option: str, key: str) -> None:
-    """Plot the results of the real data experiments."""
-    figure = SummaryFigure(xlabel="number of samples", ylabel="Power")
-    num_seeds = 1
-
-    for n in [100, 150, 200]:
-        results = Results()
-        for seed in range(num_seeds):
-            path = Path("results_real") / f"{option}_{key}_{n}_{seed}.pkl"
-            with path.open("rb") as f:
-                results += pickle.load(f)
-        assert len(results.p_values) == num_seeds * 1000
-
-        figure.add_results(results.p_values, label="proposed", xloc=n)
-        figure.add_results(results.oc_p_values, label="oc", xloc=n)
-
-    fig_path = Path("figures/real") / f"{option}_{key}.pdf"
-    fig_path.parent.mkdir(parents=True, exist_ok=True)
-    figure.plot(fig_path, fontsize=16, legend_loc="upper left")
-
-
-def print_real(option: str) -> None:
-    """Print the results of the real data experiments."""
-    keys = [
-        "heating_load",
-        "cooling_load",
-        "gas_turbine",
-        "red_wine",
-        "white_wine",
-        "abalone",
-        "concrete",
-        "housing",
-    ]
-    num_seeds = 1
-    strings_list = []
-    for n in [100, 150, 200]:
-        string = f"$n={n}$"
-        for key in keys:
-            results = Results()
-            for seed in range(num_seeds):
-                path = Path("results_real") / f"{option}_{key}_{n}_{seed}.pkl"
-                with path.open("rb") as f:
-                    results += pickle.load(f)
-            assert len(results.p_values) == num_seeds * 1000
-
-            tpr_ = rejection_rate(results.p_values, alpha=0.05)
-            oc_tpr_ = rejection_rate(results.oc_p_values, alpha=0.05)
-            tpr = Decimal(str(tpr_)).quantize(
-                Decimal("0.01"),
-                "ROUND_HALF_UP",
-            )
-            oc_tpr = Decimal(str(oc_tpr_)).quantize(
-                Decimal("0.01"),
-                "ROUND_HALF_UP",
-            )
-            string += rf" & \textbf{{{str(tpr)[1:]}}}/{str(oc_tpr)[1:]}"
-        strings_list.append(string)
-
-    strings_path = Path(f"figures/real/{option}.txt")
-    strings_path.parent.mkdir(parents=True, exist_ok=True)
-    with strings_path.open("w") as f:
-        f.write("\n".join(strings_list))
-
-
-def plot_robust_non_gaussian(option: str, alpha: float = 0.05) -> None:
-    """Plot the results of the robustness experiments for non-Gaussian noise."""
-    num_seeds = 5
-    figure = SummaryFigure(xlabel="Wasserstein Distance", ylabel="Type I Error Rate")
-
-    for rv_name in ["skewnorm", "exponnorm", "gennormsteep", "gennormflat", "t"]:
-        for distance in [0.01, 0.02, 0.03, 0.04]:
-            results = Results()
-            for seed in range(num_seeds):
-                path = (
-                    Path(f"results_robust/{rv_name}")
-                    / f"{option}_{distance}_{seed}.pkl"
-                )
-                with path.open("rb") as f:
-                    results += pickle.load(f)
-            assert len(results.p_values) == num_seeds * 1000
-            figure.add_results(
-                results.p_values,
-                label=rv_name,
-                xloc=distance,
-                alpha=alpha,
-            )
-    figure.add_red_line(value=alpha, label="significance level")
-
-    default_alpha = 0.05
-
-    fig_path = Path("figures/robust") / f"{option}_non_gaussian_{alpha:.2f}.pdf"
-    fig_path.parent.mkdir(parents=True, exist_ok=True)
-    figure.plot(
-        fig_path,
-        fontsize=13,
-        legend_loc="upper left",
-        ylim=(0.0, 0.2) if alpha == default_alpha else (0.0, 0.04),
-        yticks=[0.0, 0.05, 0.10, 0.15, 0.2]
-        if alpha == default_alpha
-        else [0.0, 0.01, 0.02, 0.03, 0.04],
-    )
-
-
-def plot_robust_estimated(option: str, mode: str) -> None:
-    """Plot the results of the robustness experiments for estimated variance."""
-    match mode:
-        case "n":
-            values = [100, 200, 300, 400]
-            result_name = lambda value, seed: f"{option}_{value}_20_{seed}.pkl"
-            fig_path = Path("figures/robust") / f"{option}_estimated_n.pdf"
-            xlabel = "number of samples"
-        case "d":
-            values = [10, 20, 30, 40]
-            result_name = lambda value, seed: f"{option}_200_{value}_{seed}.pkl"
-            xlabel = "number of features"
-
-    num_seeds = 5
-    figure = SummaryFigure(xlabel=xlabel, ylabel="Type I Error Rate")
-    for value in values:
-        results = Results()
-        for seed in range(num_seeds):
-            path = Path("results_robust/estimated") / result_name(value, seed)
-            with path.open("rb") as f:
-                results += pickle.load(f)
-        assert len(results.p_values) == num_seeds * 1000
-
-        for alpha in [0.05, 0.01, 0.10]:
-            figure.add_results(
-                results.p_values,
-                label=f"alpha={alpha:.2f}",
-                xloc=value,
-                alpha=alpha,
-            )
-
-    figure.add_red_line(value=0.05, label="significance levels")
-    figure.add_red_line(value=0.01)
-    figure.add_red_line(value=0.10)
-
-    fig_path = Path("figures/robust") / f"{option}_estimated_{mode}.pdf"
-    fig_path.parent.mkdir(parents=True, exist_ok=True)
-    figure.plot(
-        fig_path,
-        fontsize=13,
-        legend_loc="upper left",
-        ylim=(0.0, 0.2),
-        yticks=[0.0, 0.01, 0.05, 0.10, 0.15, 0.2],
-    )
-
-
 if __name__ == "__main__":
     with ProcessPoolExecutor(max_workers=32) as executor:
-        for arg in product(["op1", "op2", "all_cv"], ["n", "d", "delta"]):
-            executor.submit(plot_main, *arg)
+        for option, mode in product(["op1", "op2"], ["delta"]):  # TODO(shirara):
+            executor.submit(plot_main, *(option, mode))
+        # for arg in product(
+        #     ["all_cv"],
+        #     [
+        #         "heating_load",
+        #         "cooling_load",
+        #         "gas_turbine",
+        #         "red_wine",
+        #         "white_wine",
+        #         "abalone",
+        #         "concrete",
+        #         "housing",
+        #     ],
+        # ):
+        #     executor.submit(plot_real, *arg)
+        # executor.submit(print_real, "all_cv")
+        # for arg_ in [("all_cv", 0.05), ("all_cv", 0.01)]:
+        #     executor.submit(plot_robust_non_gaussian, *arg_)
+        # for arg in product(["all_cv"], ["n", "d"]):
+        #     executor.submit(plot_robust_estimated, *arg)
         for mode in ["n", "d"]:
             executor.submit(plot_time, mode)
-        for arg in product(
-            ["all_cv"],
-            [
-                "heating_load",
-                "cooling_load",
-                "gas_turbine",
-                "red_wine",
-                "white_wine",
-                "abalone",
-                "concrete",
-                "housing",
-            ],
-        ):
-            executor.submit(plot_real, *arg)
-        executor.submit(print_real, "all_cv")
-        for arg_ in [("all_cv", 0.05), ("all_cv", 0.01)]:
-            executor.submit(plot_robust_non_gaussian, *arg_)
-        for arg in product(["all_cv"], ["n", "d"]):
-            executor.submit(plot_robust_estimated, *arg)
