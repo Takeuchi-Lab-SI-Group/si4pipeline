8c31a476c17405bd082469734b25cae9376f0842
shirara1016 <shirara1016@gmail.com>
2024-09-01 12:25:37 +0900

    update shell file

--- shell/exp_time.sh
+++ shell/exp_time.sh
@@ -2,10 +2,10 @@
 
 file="experiment_time.py"
 
-ns=(400 800 800 800 800 1200 1600)
-ds=(80  40  80  120 160   80   80)
+ns=(1600 1200 800 800 800 800 400)
+ds=(  80   80 160 120  80  40  80)
 
-for option in op1 op1_parallel op1_serial; do
+for option in op1_serial op1_parallel op1; do
     for i in ${!ns[@]}; do
         n=${ns[i]}
         d=${ds[i]}
